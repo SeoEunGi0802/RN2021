5a62afdc6e01f9ad0cf41a27d5e07d0d14da67cc
SeoEunGi0802 <lolove08@naver.com>
2021-06-04 15:14:19 +0900

    0604 업로드

--- RN2021/app(past)/ch8/src/reducers/bookReducer8-12.js
+++ RN2021/app(past)/ch8/src/reducers/bookReducer8-12.js
@@ -1,5 +1,5 @@
 import uuidV4 from 'uuid/v4'
-import { ADD_BOOK, REMOVE_BOOK } from '../actions'
+import { ADD_BOOK } from '../actions'
 
 const initialState = {
     books: [{ name: 'East of Eden', author: 'John Steinbeck', id: uuidV4() }]
@@ -12,15 +12,6 @@ const bookReducer = (state = initialState, action) => {
                 books: [
                     ...state.books,
                     action.book
-                ]
-            }
-        case REMOVE_BOOK:
-            const index = state.books.findIndex(
-                book => book.id === action.book.id)
-            return {
-                books: [
-                    ...state.books.slice(0, index),
-                    ...state.books.slice(index + 1)
                 ]
             }
         default:
